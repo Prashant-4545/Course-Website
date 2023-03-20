3a23cc3688ac16c35dfe0cb4fc4673bed61e878c
Shlok1999 <09shlok1999ae1@gmail.com>
2023-03-20 12:59:43 +0530

    user auth done

--- server/router/user/user.route.js
+++ server/router/user/user.route.js
@@ -20,8 +20,40 @@ router.post('/signup', (req, res)=>{
     else{
         res.send({message: 'Something Went Wrong'})
     }
+});
 
-    
+router.post('/login',(req, res)=>{
+    const {email, password} = req.body;
+    if(req.body){
+        connection.query(`select * from user where email = ? and password = ?`,
+            [email, password], (err, result) => {
+                if (err) {
+                    res.send(err);
+                }
+                if (result) {
+                    try{
+                        jwt.sign({result}, jwt_key,{expiresIn: "2d"},(err, token)=>{
+                            if(err){
+                                res.send("User Not Found");
+                            }
+                            res.send({message: 'Login Successful',result: result[0], auth: token});
+                            console.log(result)
+
+                        })
+                        
+                        // res.redirect('/school-dashboard');
+                        console.log("You are Logged in")
+                    }
+                    catch(err){
+                        res.send({message: "Invalid Credentials"});
+                    }
+
+                }
+                else {
+                    res.send("Invalid Username or Password")
+                }
+            })
+    }
 })
 
 module.exports = router;
